93ae84f9e873d1256c0c2981acdf7375cd3cf5f8
Benedict <Benedictnnaoma0@gmail.com>
2023-08-07 22:56:17 +0100

    react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
